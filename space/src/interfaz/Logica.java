fca69a0af09a5e55e22ff47a4eb65675a8e81140
Cristian Salguero <david_salinasx10@hotmail.com>
2019-04-26 09:37:56 -0500

    Cambios

--- space/src/interfaz/Logica.java
+++ space/src/interfaz/Logica.java
@@ -2,7 +2,12 @@ package interfaz;
 
 import java.util.ArrayList;
 
+import pantalla.CPantalla;
+import pantalla.IPantalla;
+import pantalla.IPantallaB;
+import pantalla.IPantallaC;
 import pantalla.Img;
+import pantalla.Pantalla;
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PVector;
@@ -13,105 +18,29 @@ import space.Personaje;
 public class Logica extends Thread {
 
 	private PApplet app;
-	private Personaje personaje;
-	private ArrayList<Enemigo> enemigos;
-	private int pantalla, mapY, map2Y;
 	public static Img img;
-	private PImage stage;
-	int contador;
-	public boolean isPlay = true;
+	private Pantalla pantalla;
+	private ArrayList<CPantalla> pantallas;
+	private ArrayList<Enemigo> enemigos;
 
 	public Logica() {
 		this.app = Main.app;
-		app.imageMode(app.CENTER);
+		img = new Img();
 		this.enemigos = new ArrayList<>();
-	
-		new Thread((Nave) personaje).start();
-		this.img = new Img();
-		
-		pantalla = 6;
-	
-		int r = (int) app.random(1,4);
-		personaje = new Nave(new PVector(app.width / 2, app.height - 100), r);
-		Enemigo.r = r;
-		
-		switch (r) {
-		case 1:
-			stage = Img.loadImage("Amor.png");
-			break;
-		case 2:
-			stage = Img.loadImage("Hogar.png");
-			break;
-			
-		case 3:
-			stage = Img.loadImage("Heroes.png");
-			break;
-		}
+		this.pantallas = new ArrayList();
+
+		this.pantallas.add(inicio);
+		this.pantallas.add(juego);
 		
 		
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 3; j++) {
-					Enemigo temp = new Enemigo(new PVector(170*(i+1), 50 - (120*(j))));
-					enemigos.add(temp);
-					temp.start();
-					contador = 0;
-			}
-		}
-		mapY = app.height;
-		mapY = mapY-(stage.height/2);
+
+		this.pantalla = new Pantalla(pantallas);
+
+		app.imageMode(app.CENTER);
 	}
 
 	public void draw() {
-		if (pantalla == 6) {
-			app.image(stage, app.width/2, mapY-stage.height+10);
-			app.image(stage, app.width/2, mapY);
-			personaje.draw();
-			for (int i = 0; i < enemigos.size(); i++) {
-				enemigos.get(i).draw();
-			}
-		}
-	}
 
-	public void update() {
-
-		for (int i = 0; i < enemigos.size(); i++) {
-			if (!enemigos.get(i).isIsalive()) {
-				enemigos.remove(i);
-				return;
-			}
-		}
-		
-		mapY+= 5;
-		map2Y+= 5;
-		
-		if (mapY >= stage.height) {
-			mapY = map2Y-(stage.height/2);
-		}
-		
-		if (map2Y >= stage.height) {
-			map2Y = mapY-(stage.height/2);
-		}
-		
-		for (int i = 0; i < enemigos.size(); i++) {
-			if (enemigos.get(i).getY() > app.height-100) {
-				isPlay = false;
-				pantalla = 7;
-			}
-		}
-	}
-
-	@Override
-	public void run() {
-		while (isPlay) {
-			update();
-
-			try {
-				sleep(21);
-			} catch (InterruptedException e) {
-	
-				e.printStackTrace();
-			}
-		}
 	}
 
 	public void mousePressed() {
@@ -123,15 +52,179 @@ public class Logica extends Thread {
 	}
 
 	public void keyPressed() {
-		((Nave) personaje).keyboardPressed();
+		
 	}
 
 	public void keyReleased() {
-		((Nave) personaje).keyboardReleased();
+		
 	}
-	
+
 	public ArrayList<Enemigo> getEnemigos() {
 		return this.enemigos;
 	}
+
+	CPantalla inicio = new CPantalla("inicio", new IPantallaB() {
+		
+		PImage fondo;
+		
+		@Override
+		public void setup() {
+			this.fondo = img.loadImage("img/inicio.png");
+			
+		}
+		
+		@Override
+		public void settings() {
+			
+			
+		}
+		
+		@Override
+		public void mouseReleased() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void mousePressed() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void draw() {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+
+	CPantalla juego = new CPantalla("juego", new IPantalla() {
+
+		private Nave personaje;
+	
+		private int mapY, map2Y;
+		private PImage stage;
+		int contador;
+		public boolean isPlay = true;
+
+		@Override
+		public void setup() {
+
+			new Thread((Nave) personaje).start();
+		
+
+			int r = (int) app.random(1, 4);
+			personaje = new Nave(new PVector(app.width / 2, app.height - 100), r);
+			Enemigo.r = r;
+
+			switch (r) {
+			case 1:
+				stage = Img.loadImage("Amor.png");
+				break;
+			case 2:
+				stage = Img.loadImage("Hogar.png");
+				break;
+
+			case 3:
+				stage = Img.loadImage("Heroes.png");
+				break;
+			}
+
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 3; j++) {
+					Enemigo temp = new Enemigo(new PVector(170 * (i + 1), 50 - (120 * (j))));
+					enemigos.add(temp);
+					temp.start();
+					contador = 0;
+				}
+			}
+			mapY = app.height;
+			mapY = mapY - (stage.height / 2);
+
+		}
+
+		@Override
+		public void settings() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseReleased() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mousePressed() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void draw() {
+				app.image(stage, app.width / 2, mapY - stage.height + 10);
+				app.image(stage, app.width / 2, mapY);
+				personaje.draw();
+				for (int i = 0; i < enemigos.size(); i++) {
+					enemigos.get(i).draw();
+				}
+			
+
+			/*
+			 * 
+			 * 
+			 */
+
+			for (int i = 0; i < enemigos.size(); i++) {
+				if (!enemigos.get(i).isIsalive()) {
+					enemigos.remove(i);
+					return;
+				}
+			}
+
+			mapY += 5;
+			map2Y += 5;
+
+			if (mapY >= stage.height) {
+				mapY = map2Y - (stage.height / 2);
+			}
+
+			if (map2Y >= stage.height) {
+				map2Y = mapY - (stage.height / 2);
+			}
+
+			for (int i = 0; i < enemigos.size(); i++) {
+				if (enemigos.get(i).getY() > app.height - 100) {
+					isPlay = false;
+					
+					
+					/*
+					 * 
+					 * 
+					 * Siiguiente pantalla
+					 */
+				}
+			}
+		}
+
+		@Override
+		public void mouseMouseDraged() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyReleased() {
+			this.personaje.keyboardReleased();
+		}
+
+		@Override
+		public void keyPressed() {
+			this.personaje.keyboardPressed();
+
+		}
+
+	});
 
 }
